9cab54cc8007e1920b24504e4014a75d6965afe5
MerriUtami16 <93017835+MerriUtami16@users.noreply.github.com>
2022-02-26 20:43:26 +0700

    Add files via upload

---
+++ Pertemuan 1 PW2/variabel konstan.php
@@ -0,0 +1,8 @@
+<?php
+  // Variable konstan
+  // adalah variable yang tidak bisa diubah nilainya
+  define('makanan', 'mie goreng');
+  // define('makanan', 'mangga');
+  echo '<h1 style="color: brown">'.makanan.'</h1>';
+  echo makanan;
+?>
---
+++ Pertemuan 1 PW2/variabel user.php
@@ -0,0 +1,14 @@
+<?php
+// variabel user 
+$nama = 'merri';
+$umur = 20 ;
+$berat = 58;
+
+echo 'Nama : ' .$nama;
+echo '<br/>Umur : ' .$umur. 'Tahun';
+echo '<br/>Berat : ' .$berat. ' Kg ';
+
+echo "<br/> Hello $nama Apakabar";
+echo "<br/> Hai nama saya $nama Umur saya $umur Berat badan saya $berat Kg, Salam kenal semua nya";
+echo "<hr>";
+?>
